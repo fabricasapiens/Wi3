414eb22d9d3b090cfaae3368279bff750feaace8
Fabrica Sapiens <info@fabricasapiens.nl>
2011-03-31 22:22:22 +0200

    Simplified base class

--- app/latest/classes/wi3/base.php
+++ app/latest/classes/wi3/base.php
@@ -1,13 +1,9 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php
 
-    // Class makes sure that extending objects have the instance() function (and its alias inst())
-    // That return an instance of the object. Instances can be given names, and constructparameters
     class Wi3_Base
     {
-        
-        // Object and array that keep the instances of this class
-        public static $_instance = NULL;
         public static $_instances = Array();
+        public static $_instance;
         
         // Function returns instance of this class
         // Instances can also be labeled with a name, to have multiple 'singleton' instances of a class
@@ -20,17 +16,8 @@
             if ($name == NULL) {
                 if (!(static::$_instance instanceof static)) // Caution: Late Static Binding, only to be found in PHP 5.3.0. 'static' refers to the calling Class that extends Wi3_Base
                 {
-                    // Create new instance (with the constructparameters, if any)
-                    if ($constructparameters !== NULL) 
-                    {
-                        $new = new static($constructparameters);
-                        static::$_instance = & $new; // Pass by reference, so that working with an instance updates back to here (so that the next instance() call will have the updated object)
-                    } 
-                    else 
-                    {
-                        $new = new static;
-                        static::$_instance = & $new; // Pass by reference
-                    }
+                    $new = new static($constructparameters); // If no $constructparameters were sent along, then sending NULL will have the same effect, since isset($param) will in both cases return FALSE
+                    static::$_instance = & $new; // Pass by reference, so that working with an instance updates back to here (so that the next instance() call will have the updated object)
                 }
                 return static::$_instance;
             } 
@@ -38,21 +25,12 @@
             {
                 if (!isset(static::$_instances[$name]) OR !(static::$_instances[$name] instanceof static))
                 {
-                    // Create new instance (with the constructparameters, if any)
-                    if ($constructparameters !== NULL) 
-                    {
-                        $new = new static($constructparameters);
-                        static::$_instances[$name] = & $new; // Pass by reference, so that working with an instance updates back to here (so that the next instance() call will have the updated object)
-                    } 
-                    else 
-                    {
-                        $new = new static;
-                        static::$_instances[$name] = & $new; // Pass by reference
-                    }
+                    $new = new static($constructparameters); // If no $constructparameters were sent along, then sending NULL will have the same effect, since isset($param) will in both cases return FALSE
+                    static::$_instances[$name] = & $new; // Pass by reference, so that working with an instance updates back to here (so that the next instance() call will have the updated object)
                 }
                 return static::$_instances[$name];
             }
         }
     }
-
+    
 ?>
