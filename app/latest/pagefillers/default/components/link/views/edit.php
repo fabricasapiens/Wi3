b3c5365ce290703ded62699de16b2a55766f255b
Fabrica Sapiens <info@fabricasapiens.nl>
2012-07-04 21:12:57 +0200

    link up~dates

--- app/latest/pagefillers/default/components/link/views/edit.php
+++ app/latest/pagefillers/default/components/link/views/edit.php
@@ -7,7 +7,7 @@
 
         echo Wi3::inst()->formbuilder->input()->attr("name", "url")->attr("value", $data->url)->render();
 		
-		echo "<button onclick='wi3.request(\"pagefiller_default_component_link/edit\", {fieldid: \"" . $field->id . "\", url: $(this).closest('form').find(\"[name=url]\").val()});'>url gebruiken</button>";
+		echo "<button onclick='wi3.request(\"pagefiller_default_component_link/edit\", {fieldid: \"" . $field->id . "\", url: $(this).closest(\"form\").find(\"[name=url]\").val()});'>url gebruiken</button>";
 		echo "<p>Of klik op een van onderstaande bestanden om een link naar dat bestand te maken.</p>";
 		echo "<div onClick='wi3.request(\"pagefiller_default_component_link/edit\", {fieldid: \"" . $field->id . "\", fileid: $(this).find(\"[name=file]\").val()});'>";
 			if (!isset($data->fileid)) {
