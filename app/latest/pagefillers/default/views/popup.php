d6f05702b2a3b618eddd11f576fa8b44b327deaa
Willem Mulder <ontwerpen@gmail.com>
2012-08-29 23:01:02 +0200

    Small visual update to adminarea popup

--- app/latest/pagefillers/default/views/popup.php
+++ app/latest/pagefillers/default/views/popup.php
@@ -1,4 +1,4 @@
 <div type='popup' style='position: fixed; z-index: 10001; width: 600px; left: 50%; top: 30px; bottom: 30px; background: #fff; margin-left: -300px; color: #000; display: none; -webkit-box-shadow: 0px 0px 15px #777;'>
-    <div type='popuphtml' style='position: absolute; top: 20px; left: 20px; right: 20px; bottom: 60px; overflow: auto;'></div>
+    <div type='popuphtml' style='position: absolute; top: 10px; left: 10px; right: 10px; bottom: 60px; overflow: auto; padding: 10px;'></div>
     <div style='padding: 20px; height: 20px;  position: absolute; bottom: 0px;'><button onclick='wi3.pagefillers.default.edittoolbar.hidePopup();'>Sluiten</button></div>
 </div>
