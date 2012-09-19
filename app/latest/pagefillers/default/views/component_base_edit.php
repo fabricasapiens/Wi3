450acce9adede9200e982bea8d51ff31e0d4163e
Willem Mulder <ontwerpen@gmail.com>
2012-09-19 22:48:13 +0200

    Make fields able to opt-out for inclusion in the default editpage layout

--- app/latest/pagefillers/default/views/component_base_edit.php
+++ app/latest/pagefillers/default/views/component_base_edit.php
@@ -8,50 +8,55 @@
 		$jscode = "";	
 		// Loop over every item of the model and create a corresponding input
 		foreach($model as $name => $info) {
-			echo "<p><label for='" . $name . "' class='mediumpadding'>" . (isset($info->label) ? $info->label : $name) . "</label></p><p class='mediumpadding'>";
-			// Fetch current value
-			$currentvalue = $data->{$name};
-			// Render input
-			if ($info["type"] == "text") {
-				if (isset($info["length"]) && (!is_numeric($info["length"]) || $info["length"] == 0 || $info["length"] > 50)) {
-					$input = Wi3::inst()->formbuilder->textarea();
-				} else {
-					$input = Wi3::inst()->formbuilder->input();
-				}
-			} else if ($info["type"] == "date") {
-				$input = Wi3::inst()->formbuilder->dateselector();
-			} else if ($info["type"] == "image") {
-				$input = Wi3::inst()->formbuilder->fileselector();
-				if (isset($info["extensions"]) AND is_array($info["extensions"])) {
-					$input->set("fileextensions", $info["extensions"]);
-				}
-			} else if ($info["type"] == "file") {
-				$input = Wi3::inst()->formbuilder->fileselector();
-				if (isset($info["extensions"]) AND is_array($info["extensions"])) {
-					$input->set("fileextensions", $info["extensions"]);
-				}
-			} else if ($info["type"] == "folder") {
-				$input = Wi3::inst()->formbuilder->folderselector();
-				// Possibly only show folders that contain certain filetypes
-				if (isset($info["extensions"]) AND is_array($info["extensions"])) {
-					$input->set("fileextensions", $info["extensions"]);
-				}
-			} else if ($info["type"] == "number") {
-				$input = Wi3::inst()->formbuilder->input();
-			} else if ($info["type"] == "list") {
-				// TODO: lists of elements with add/remove/move etc
-				// TODO: some lists like tags have a special list-interface
-				$input = Wi3::inst()->formbuilder->input();
+			if (isset($info["showoneditscreen"]) && $info["showoneditscreen"] === false) {
+
 			} else {
-				die("the '" . $info["type"] . "' type of input is not supported");
+
+				echo "<p><label for='" . $name . "' class='mediumpadding'>" . (isset($info->label) ? $info->label : $name) . "</label></p><p class='mediumpadding'>";
+				// Fetch current value
+				$currentvalue = $data->{$name};
+				// Render input
+				if ($info["type"] == "text") {
+					if (isset($info["length"]) && (!is_numeric($info["length"]) || $info["length"] == 0 || $info["length"] > 50)) {
+						$input = Wi3::inst()->formbuilder->textarea();
+					} else {
+						$input = Wi3::inst()->formbuilder->input();
+					}
+				} else if ($info["type"] == "date") {
+					$input = Wi3::inst()->formbuilder->dateselector();
+				} else if ($info["type"] == "image") {
+					$input = Wi3::inst()->formbuilder->fileselector();
+					if (isset($info["extensions"]) AND is_array($info["extensions"])) {
+						$input->set("fileextensions", $info["extensions"]);
+					}
+				} else if ($info["type"] == "file") {
+					$input = Wi3::inst()->formbuilder->fileselector();
+					if (isset($info["extensions"]) AND is_array($info["extensions"])) {
+						$input->set("fileextensions", $info["extensions"]);
+					}
+				} else if ($info["type"] == "folder") {
+					$input = Wi3::inst()->formbuilder->folderselector();
+					// Possibly only show folders that contain certain filetypes
+					if (isset($info["extensions"]) AND is_array($info["extensions"])) {
+						$input->set("fileextensions", $info["extensions"]);
+					}
+				} else if ($info["type"] == "number") {
+					$input = Wi3::inst()->formbuilder->input();
+				} else if ($info["type"] == "list") {
+					// TODO: lists of elements with add/remove/move etc
+					// TODO: some lists like tags have a special list-interface
+					$input = Wi3::inst()->formbuilder->input();
+				} else {
+					die("the '" . $info["type"] . "' type of input is not supported");
+				}
+				$input->attr(Array("name" => $name, "class" => "fullwidth"));
+				$input->val($currentvalue);
+				echo $input->render();
+				// Js code to fetch the element again when user submits
+				$jscode .= ", " . $name . ": $(this).prevAll(\"form\").find(\"*[name=" . $name . "]\").val()";
+				echo "</p>";
 			}
-			$input->attr(Array("name" => $name, "class" => "fullwidth"));
-			$input->val($currentvalue);
-			echo $input->render();
-			// Js code to fetch the element again when user submits
-			$jscode .= ", " . $name . ": $(this).prevAll(\"form\").find(\"*[name=" . $name . "]\").val()";
 		}
-        echo "</p>";
     
     echo "</form>";
 
