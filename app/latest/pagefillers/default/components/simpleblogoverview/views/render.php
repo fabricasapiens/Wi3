3fbe6455654193426aa8a93d34648262cf1f4355
Willem Mulder <ontwerpen@gmail.com>
2012-09-21 22:53:08 +0200

    Correct date for blogoverview

--- app/latest/pagefillers/default/components/simpleblogoverview/views/render.php
+++ app/latest/pagefillers/default/components/simpleblogoverview/views/render.php
@@ -6,8 +6,8 @@
 		foreach($articles as $index => $article) {
 			echo "<a href='" . $article->pageurl . "' style='display: block;'>";
 				echo "<div>";
-					echo "<span style='float: right;'>" . date("Y-m-d H:i", (int)$data->entertimestamp)  . 
-						(!empty($data->edittimestamp) ? " (updated " . date("Y-m-d H:i", (int)$data->edittimestamp) . ")" : "") . 
+					echo "<span style='float: right;'>" . date("Y-m-d H:i", (int)$article->entertimestamp)  . 
+						(!empty($article->edittimestamp) ? " (updated " . date("Y-m-d H:i", (int)$article->edittimestamp) . ")" : "") . 
 					"</span>";
 					echo "<h2>" . $article->title . "</h2>";
 					echo "<img style='float: left; margin-bottom: 10px; margin-right: 10px;' src='" . $article->imageurl .  "'/>";
