5e9640e927f06b4a62a9e01d47fbd26c9b2a0120
Willem Mulder <ontwerpen@gmail.com>
2012-10-09 23:20:02 +0200

    small mail fix

--- app/latest/pagefillers/default/components/contactform/views/mail.php
+++ app/latest/pagefillers/default/components/contactform/views/mail.php
@@ -1,4 +1,4 @@
-Naam : <?php echo $data["name"]; ?>
-Email : <?php echo $data["emailaddress"]; ?>
-Onderwerp : <?php echo $data["subject"]; ?>
+Naam : <?php echo $data["name"]; ?>\r\n
+Email : <?php echo $data["emailaddress"]; ?>\r\n
+Onderwerp : <?php echo $data["subject"]; ?>\r\n
 Bericht : <?php echo $data["message"]; ?>
