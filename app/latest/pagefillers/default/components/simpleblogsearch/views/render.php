b256a001343e93b9393708ec0ed2e762703fa406
Willem Mulder <ontwerpen@gmail.com>
2012-10-19 18:30:15 +0200

    Improved styling

--- app/latest/pagefillers/default/components/simpleblogsearch/views/render.php
+++ app/latest/pagefillers/default/components/simpleblogsearch/views/render.php
@@ -28,7 +28,7 @@
 
 ?>
 
-<input style='border-radius: 4px; padding: 4px; font-height: 1.5em;' class='wi3_pagefiller_default_component_simpleblogsearch' placeholder='zoeken'></input>
+<input class='wi3_pagefiller_default_component_simpleblogsearch' placeholder='zoeken'></input>
 <div style='background: #fff; max-width: 200px;'>
 	<div class='wi3_pagefiller_default_component_simpleblogsearch_result'>
 		...
