0314100f345be4bd5881edb448d504c8847a1289
willemmulder <willemmulder@gmail.com>
2013-04-19 12:06:08 +0200

    Remove border around Github gist. It doesn't show up properly when floated content is on the left of the gist.

--- app/latest/pagefillers/default/components/githubgist/views/render.php
+++ app/latest/pagefillers/default/components/githubgist/views/render.php
@@ -1,7 +1,11 @@
 <?php
 
+	$this->css("style.css");
+
 	if (!empty($username) && !empty($id)) {
-		echo '<script src="https://gist.github.com/' . $username . '/' . $id . '.js"></script>';
+		echo "<div class='component_githubgist'>";
+			echo '<script src="https://gist.github.com/' . $username . '/' . $id . '.js"></script>';
+		echo "</div>";
 	} else {
 		echo "Gist is not yet configured.";
 	}
