afb52508f3e6cd2b98fdb53eee92960771382307
willemmulder <willemmulder@gmail.com>
2013-04-19 08:15:54 +0200

    Remove trailing spaces

--- app/latest/pagefillers/default/components/simpleblog/views/edit.php
+++ app/latest/pagefillers/default/components/simpleblog/views/edit.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
     $prefix = "component_simpleblog";
 
@@ -12,23 +12,23 @@
 	<div id="<?php echo $prefix; ?>_edit_tabs-1">
         <?php
         echo "<form onsubmit='return false;'>";
-        
+
             // New article
-            
+
             // Title
             echo Wi3::inst()->formbuilder->input()->attr("name", "title")->render();
             echo Wi3::inst()->formbuilder->input()->attr("name", "title")->render();
-            
+
             $imagedata = Wi3::inst()->model->factory("site_data")->setref($field)->setname("image")->load();
             $fileid = $imagedata->data;
             echo Wi3::inst()->formbuilder->fileselector()->attr("name", "image")->set("extensions", Array("jpg", "jpeg"))->addextension("png")->set("selected", $fileid)->render();
-        
+
         echo "</form>";
 
         echo "<button onclick='wi3.request(\"pagefiller_default_component_simpleblog/edit\", {fieldid: " . $field->id . ", image: $(this).prevAll(\"form\").find(\"input\").val()});'>geselecteerde afbeelding gebruiken</button>";
-    
+
     echo "</div>";
-    
+
     echo "<div id='" . $prefix . "_edit_tabs-2'>";
         echo "hoi";
     echo "</div>";
