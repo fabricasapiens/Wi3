552fd6387d0d05e68dce3d96f571cc88be1f9b5c
willemmulder <willemmulder@gmail.com>
2013-04-19 08:26:36 +0200

    Move isImage and getExtension to file model

--- app/latest/classes/model/site/file.php
+++ app/latest/classes/model/site/file.php
@@ -7,7 +7,7 @@
 class Model_Site_File extends Sprig_MPTT
 {
     public $_db = 'site'; // This database instance will be defined in the Wi3 setup, via Wi3_Database::instance("site");
-    
+
 	protected $_title_key = 'id';
 
 	protected $_sorting = array('lft' => 'asc');
@@ -16,12 +16,12 @@ class Model_Site_File extends Sprig_MPTT
 	{
 		$this->_fields += array(
 			'id' => new Sprig_Field_Auto,
-            
+
             'type' => new Sprig_Field_Char(array( // can be folder or file
 				'empty'  => FALSE, // can not be empty
 				'unique' => FALSE,
 			)),
-            
+
             'title' => new Sprig_Field_Char(array(
 				'empty'  => FALSE, // can not be empty
 				'unique' => FALSE,
@@ -30,7 +30,7 @@ class Model_Site_File extends Sprig_MPTT
 				'empty'  => TRUE, // can be empty, if file is a folder
 				'unique' => TRUE, // should be unique
 			)),
-			
+
             'keywords' => new Sprig_Field_Char(array(
 				'empty'  => TRUE,
 				'unique' => FALSE,
@@ -40,14 +40,14 @@ class Model_Site_File extends Sprig_MPTT
 			)),
             "lastupdated" => new Sprig_Field_Timestamp(array( // When this field was updated (use at will)
 				'empty'  => TRUE,
-			)), 
-			
+			)),
+
 			"owner" => new Sprig_Field_BelongsTo(array( // Owner of this file (default is the creator of the file)
 				'empty'  => FALSE,
                 'model' => 'User',
                 'column' => 'owner_id', // Column in the site_file-model
                 // 'foreign_key' => 'id' // Is assumed
-			)), 
+			)),
             "viewright" => new Sprig_Field_Char(array(  // What right does a user/group need to view
 				'empty'  => TRUE,
 				'unique' => FALSE,
@@ -56,19 +56,33 @@ class Model_Site_File extends Sprig_MPTT
 				'empty'  => TRUE,
 				'unique' => FALSE,
 			)),
-            "adminright" => new Sprig_Field_Char(array(   // what right does a user/group need to delete the file or edit one of the other rights. Default is 'admin' 
+            "adminright" => new Sprig_Field_Char(array(   // what right does a user/group need to delete the file or edit one of the other rights. Default is 'admin'
 				'empty'  => TRUE,
 				'unique' => FALSE,
                 'default' => 'admin'
-			)), 
-            
+			)),
+
             'lft' => new Sprig_Field_MPTT_Left,
             'rgt' => new Sprig_Field_MPTT_Right,
             'lvl' => new Sprig_Field_MPTT_Level,
             'scope' => new Sprig_Field_MPTT_Scope,
 		);
 	}
-    
+
+	public function isImage() {
+		if ($this->type !== "file") {
+			return false;
+		}
+		$extension = $this->getExtension();
+        $imageExtensions = Array("jpg", "jpeg", "png", "bmp", "gif");
+        return in_array($extension, $imageExtensions);
+	}
+
+	public function getExtension() {
+        $lastdotpos = strrpos($this->filename, ".");
+        return strtolower(substr($this->filename, $lastdotpos+1));
+	}
+
 }
-    
+
 ?>
--- app/latest/views/adminarea/files.php
+++ app/latest/views/adminarea/files.php
@@ -3,9 +3,9 @@
     // We need the UI and fancybox plugin
     Wi3::inst()->plugins->load("plugin_jquery_ui"); // For dragging and dropping
     Wi3::inst()->plugins->load("plugin_jquery_fancybox"); // For the nice modal box
-    
+
     $site = Wi3::inst()->sitearea->site;
-    
+
     echo "<div id='wi3_prullenbak'>";
         echo "<div id='prullenbak_onder'><h2>Sleep hier om te verwijderen</h2></div>";
     echo "</div>";
@@ -20,16 +20,6 @@
         echo "<a href='javascript:void(0);' onClick='adminarea.addfolder();'><h2>+</h2></a>";
     echo "</div>";
 
-    function getextension($filename) {
-        $lastdotpos = strrpos($filename, ".");
-        return strtolower(substr($filename, $lastdotpos+1));
-    }
-
-    function isimage($file) {
-        $extensions = Array("jpg", "jpeg", "png", "bmp", "gif");
-        return $file->type === "file" && in_array(getextension($file->filename), $extensions);
-    }
-    
     echo "<ul id='files_files' style='position: relative;' class='simpleTree'><li class='root'><span></span><ul>";
     // Get all files and render them in a tree
     $files = Wi3::inst()->sitearea->files->getall();
@@ -49,22 +39,22 @@
                 }
                 else if ($file->{$prevfile->level_column} < $prevfile->{$prevfile->level_column})
                 {
-                    // Going a level up, or maybe even more than 1 level 
+                    // Going a level up, or maybe even more than 1 level
                     // Find out how many levels we go up and close every level properly
                     for($i=($prevfile->{$prevfile->level_column} - $file->{$prevfile->level_column}); $i > 0; $i--)
                     {
                         echo "</li></ul></li>";
                     }
-                } 
-                else 
+                }
+                else
                 {
                     echo "</li>";
                 }
             }
             $prevfile = $file;
-            
+
             echo "<li class='treeItem " . ($file->type == "folder" ? "permanent-folder" : "") . "' id='treeItem_" . $file->id . "'>";
-            echo "<span>" . html::anchor(Wi3::inst()->urlof->site . "_uploads/" . $file->filename, (isimage($file) ? "<img src='" . Wi3::inst()->urlof->site . "_uploads/30/" . $file->filename . "'/> " : "") . $file->title) . "</span>";
+            echo "<span>" . html::anchor(Wi3::inst()->urlof->site . "_uploads/" . $file->filename, ($file->isImage() ? "<img src='" . Wi3::inst()->urlof->site . "_uploads/30/" . $file->filename . "'/> " : "") . $file->title) . "</span>";
         }
         // Now, if we have ended far from root (i.e. a deep node), we need to add some </li></ul>
         if ($file->{$prevfile->level_column} > 0)
@@ -77,13 +67,13 @@
         echo "</li></ul>";
     }
     echo "</ul>";
-    
+
     //render the container in which the page properties will appear when a page is single-clicked
     echo "<div id='files_filesettings'>";
          echo "<div id='files_filesettings_tabs'>";
-            
+
         echo "</div>";
     echo "</div>";
-    
-    
+
+
 ?>
