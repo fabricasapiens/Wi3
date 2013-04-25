a4be80f16f0ea9677fb395fca2c0c681d60f4667
willemmulder <willemmulder@gmail.com>
2013-04-25 16:34:24 +0200

    ...

--- app/latest/pagefillers/default/components/simpleblogrssfeed/views/rssfeed.php
+++ app/latest/pagefillers/default/components/simpleblogrssfeed/views/rssfeed.php
@@ -14,8 +14,8 @@
 		foreach($articles as $article) {
 			echo "<item>";
 				echo "<title><![CDATA[" . $article->title . "]]></title>";
-				echo "<link>" . rawurlencode($article->pageurl) . "</link>";
-				echo "<guid>" . rawurlencode($article->pageurl) . "</guid>";
+				echo "<link>" . str_replace("http%3A%2F%2F", "http://", rawurlencode($article->pageurl)) . "</link>";
+				echo "<guid>" . str_replace("http%3A%2F%2F", "http://", rawurlencode($article->pageurl)) . "</guid>";
 				echo "<description><![CDATA[" . $article->summary . "]]></description>";
 			echo "</item>";
 		}
