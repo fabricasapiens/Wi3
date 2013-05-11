8160d8e0f52e5dee4617b975be8a57b0c840a316
willemmulder <willemmulder@gmail.com>
2013-05-11 20:07:30 +0200

    Support Arrays as key

--- app/latest/pagefillers/default/classes/pagefiller/default/component/base.php
+++ app/latest/pagefillers/default/classes/pagefiller/default/component/base.php
@@ -1,8 +1,8 @@
 <?php
 
-    Class Pagefiller_Default_Component_Base extends Wi3_Base 
+    Class Pagefiller_Default_Component_Base extends Wi3_Base
     {
-    
+
         public static function view($viewname)
         {
             $componentname = strtolower(self::get_calling_componentname());
@@ -10,12 +10,12 @@
             $componenturl = Wi3::inst()->urlof->pagefillerfiles("default") . "components/" . $componentname . "/";
             $componentpath = Wi3::inst()->pathof->pagefiller("default") . "components/" . $componentname . "/";
             $componentbaseview = Wi3_Baseview::instance('pagefillercomponentbaseview'.$componentname, array(
-                'javascript_url' => $componenturl.'static/javascript/', 
+                'javascript_url' => $componenturl.'static/javascript/',
                 'javascript_path' => $componentpath.'static/javascript/',
                 'css_url' => $componenturl.'static/css/',
                 'css_path' => $componentpath.'static/css/',
                 'view_path' => $componentpath.'views/',
-            )); 
+            ));
             $componentview = View::factory()->set("this", $componentbaseview);
             $componentview->set_filepath($componentpath.'views/'.$viewname.EXT); // set_filepath sets a complete filename on the View
             return $componentview;
@@ -23,6 +23,7 @@
 
         // This function determines where the data of the editable blocks within this field are retrieved from
         // By default, they are loaded from the data-object that is tied to this field
+        // The model should have an item that has property "editableblockname"
         public function loadEditableBlockData($field, $blockName) {
             $model = $this->getModel();
             foreach($model as $index => $modelitem) {
@@ -37,6 +38,7 @@
 
         // This function determines where the data of the editable blocks within this component are stored
         // By default, they are stored on the data-object that is tied to this field
+        // The model should have an item that has property "editableblockname"
         public function saveEditableBlockData($field, $blockName, $content) {
             $model = $this->getModel();
             foreach($model as $index => $modelitem) {
@@ -70,11 +72,26 @@
             return $this::$model;
         }
 
+        protected function getTypeForModelKey($key) {
+        	$model = $this->getModel();
+        	if (!isset($model[$key])) {
+        		return false;
+        	}
+        	return $model[$key]["type"];
+        }
+
         protected function ensureModelExists($dataobject,$savedata=true) {
             $changed = false;
             foreach($this->getModel() as $key => $info) {
                 if (!isset($dataobject->{$key})) {
-                    $dataobject->{$key} = "";
+                	$type = $this->getTypeForModelKey($key);
+                	if ($type === "number") {
+                		$dataobject->{$key} = 0;
+                	} else if ($type === "array") {
+                		$dataobject->{$key} = Array();
+                	} else {
+                		$dataobject->{$key} = "";
+                	}
                     $changed = true;
                 }
             }
@@ -82,7 +99,7 @@
                 $dataobject->update();
             }
         }
-        
+
 		protected function fielddata($field=null, $key=null, $value=null) {
             if ($field === null) {
                 return false;
@@ -95,6 +112,7 @@
             if ($key === null) {
                 // Ensure that all elements from the model exist
                 $this->ensureModelExists($dataobject, false);
+                $this->ensureProperTypeForDataObject($dataobject);
                 return $dataobject;
             } else {
                 if (is_object($key) && $value !== null) {
@@ -112,20 +130,39 @@
                 }
             }
         }
-        
+
+        protected function ensureProperTypeForDataObject($dataobject) {
+        	foreach($this->getModel() as $key => $info) {
+                if (isset($dataobject->{$key})) {
+                	$type = $this->getTypeForModelKey($key);
+                	if ($type === "number") {
+                		$dataobject->{$key} = intval($dataobject->{$key});
+                	} else if ($type === "array") {
+                		if (is_string($dataobject->{$key})) {
+	                		$dataobject->{$key} = unserialize($dataobject->{$key});
+	                		// TODO: if type is not array, create new empty array
+                		}
+                	} else {
+                		// Ensure a string
+                		$dataobject->{$key} = strval($dataobject->{$key});
+                	}
+                }
+            }
+        }
+
         public static function css($filename)
         {
             $componentname = self::get_calling_componentname();
             Wi3::inst()->css->add(Wi3::inst()->urlof->pagefillerfiles("default") . "components/" . $componentname . "/" . "static/css/" . $filename, "view");
         }
-        
-        
+
+
         public static function javascript($filename)
         {
             $componentname = self::get_calling_componentname();
             Wi3::inst()->javascript->add(Wi3::inst()->urlof->pagefillerfiles("default") . "components/" . $componentname . "/" . "static/javascript/" . $filename, "view");
         }
-        
+
         private static function get_calling_componentname() {
             $callingClass = get_called_class(); // Uses late static binding
             $callingClassLastpart = substr($callingClass, strrpos($callingClass, "_")+1);
@@ -136,7 +173,7 @@
         {
             return "<a href='javascript:void(0)' onclick='wi3.request(\"pagefiller_default_component_" . self::get_calling_componentname() . "/startEdit\", {fieldid: " . $field->id . "})'>wijzigen</a>";
         }
-        
+
     }
-    
+
 ?>
