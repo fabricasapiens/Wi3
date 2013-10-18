1937e588595b458eda19e8b7976fb99acfcdc4f7
Willem Mulder <willemmulder@gmail.com>
2013-10-18 09:37:12 +0200

    Labels for the inputs on contactform

--- app/latest/pagefillers/default/components/contactform/views/render.php
+++ app/latest/pagefillers/default/components/contactform/views/render.php
@@ -7,9 +7,13 @@
 
 	echo "<h2 style='width: 100%;'>Contactformulier</h2>";
 	
+	echo "<label>Naam</label>";
 	echo "<input name='name' placeholder='naam' style='width: 100%;'/>";
+	echo "<label>Emailadres</label>";
 	echo "<input name='emailaddress' type='email' placeholder='emailadres' style='width: 100%;'/>";
+	echo "<label>Onderwerp</label>";
 	echo "<input name='subject' placeholder='onderwerp' style='width: 100%;'/>";
+	echo "<label>Bericht</label>";
 	echo "<textarea name='message' placeholder='bericht' style='width: 100%; height: 300px;'/>";
 	echo "<button onclick='wi3.pagefiller[\"default\"].component.contacform.submit(this);'>Verzenden</button>";
 
